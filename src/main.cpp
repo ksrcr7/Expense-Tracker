f1458622fdfbaac67701ca2b0711c14a6a7444ce
ksrcr7 <kasrabakhshaei25@gmail.com>
2025-12-26 22:48:57 +0330

    Add demo structure for running program

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,37 @@
+#include <expense_tracker/storage/InMemoryExpenseStorage.h>
+#include <expense_tracker/app/ExpenseService.h>
+#include <expense_tracker/cli/CommandHandler.h>
+
 #include <iostream>
+#include <string>
+#include <stdexcept>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    expense_tracker::storage::InMemoryExpenseStorage InMemoSto;
+    expense_tracker::app::ExpenseService ExpService(InMemoSto);
+    expense_tracker::cli::CommandHandler Command(ExpService);
+
+    std::cout<<"Expense Tracker (type 'help')\n";
+
+    while (true){
+        std::cout<<"> ";
+        std::string line;
+        if(!std::getline(std::cin,line))break;
+
+        try{
+            Command.handle(line);
+        }
+        catch (std::runtime_error& e) {
+            if(std::string (e.what()) == "exit")break;
+            std::cout<<"Error: "<<e.what()<<std::endl;
+        }
+        catch (const std::invalid_argument& e) {
+            std::cout<<e.what()<<std::endl;
+        }
+        catch (const std::exception& e) {
+            std::cout<<"Error: "<<e.what()<<std::endl;
+        }
+    }
+    std::cout<<"Bye!\n";
     return 0;
 }
